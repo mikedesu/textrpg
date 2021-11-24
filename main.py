65af44c23c6fe116d8f40f2b8aa4e10f88acc65a
darkmage <165251+mikedesu@users.noreply.github.com>
2021-11-24 01:14:10 -0600

    sliding past 666 LOC

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 from sys import exit
 from curses import wrapper
 from tools import get_user_input_ch, \
-    get_player_name, get_player_race, get_player_job, \
-    translate_race_str_to_enum, translate_job_str_to_enum, \
-    generate_random_stats, handle_new_game_stats,  \
-    new_character, handle_input, quick_new_character 
-import Game
-import Renderer
+    get_player_name, \
+    get_player_race, \
+    get_player_job, \
+    translate_race_str_to_enum, \
+    translate_job_str_to_enum, \
+    generate_random_stats, \
+    handle_new_game_stats,  \
+    new_character, \
+    handle_input, \
+    quick_new_character 
+import Game, Renderer
 
 def game_loop(game, renderer, pc):
     while True:
@@ -35,10 +40,9 @@ def main(stdscr):
     renderer.draw_titlescreen()
     cc = get_user_input_ch(stdscr, ['n', 'q'])
     if cc=='n':
-        #pc = new_character(renderer.s)
         pc = quick_new_character(renderer.s)
-        pc.y = 0 # when drawing pc or anything in dungeon, have to account for offset of borders
-        pc.x = 0
+        pc.y = 0
+        pc.x = 0 # when drawing pc or anything in dungeon, have to account for offset of borders
         game_loop(game, renderer, pc)
     elif cc=='q':
         exit(0)
