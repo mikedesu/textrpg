2f10dee434f44dc3a310b03c6d154e4ed9759271
darkmage <165251+mikedesu@users.noreply.github.com>
2021-12-08 01:05:58 -0600

    added weight to item properties

--- Game/Item.py
+++ Game/Item.py
@@ -3,7 +3,7 @@ from .ItemClass import ItemClass
 
 
 class Item:
-    def __init__(self, name="Unnamed Item", itemclass=ItemClass.WEAPON, x=0, y=0 ):
+    def __init__(self, name='Unnamed Item', itemclass=ItemClass.WEAPON, x=0, y=0, weight=0, symbol='*' ):
         assert(name != "")
         assert(name != None)
         assert(itemclass != None) 
@@ -11,9 +11,9 @@ class Item:
         self.itemclass = itemclass 
         self.x = x
         self.y = y
-
+        self.weight = weight
         # future items will have unique symbols based on itemclass and other factors
-        self.symbol = "*"
+        self.symbol = symbol 
 
     def __str__(self):
         return self.name 
