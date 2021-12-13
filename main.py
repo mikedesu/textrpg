48a8ce7692cea41195f78d1d5af9ce5bd9c5db2a
darkmage <165251+mikedesu@users.noreply.github.com>
2021-12-13 03:13:48 -0600

    commented-out a bunch of lines in tools

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
 #!/usr/bin/python3
 from sys import exit
 from curses import wrapper
-
 import Game
 from Game.Renderer import Renderer
-
 from tools import get_user_input_ch
-from tools import get_player_name
-from tools import get_player_race
-from tools import get_player_job
-from tools import translate_race_str_to_enum
-from tools import translate_job_str_to_enum
-from tools import generate_random_stats
-from tools import handle_new_game_stats
-from tools import new_character
 from tools import quick_new_character 
 
 def game_loop(game):
     while True:
         game.renderer.draw_main_screen(game)
-        #game.renderer.draw_main_screen(game, game.pc)
         key = game.renderer.s.getkey()
-        ##################################################################
-        # what are our movement keys by default?                         #
-        # Left-handed mode:                                              #
-        # A  S  D  F                                                     #
-        # Left  = A J Left                                               #
-        # Up    = S K Up                                                 #
-        # Down  = D L Down                                               #
-        # Right = F ; Right                                              #
-        ##################################################################
         rows, cols = game.renderer.s.getmaxyx()
         do_incr_turns = game.handle_input(game.pc, key)
         if do_incr_turns:
@@ -44,13 +24,10 @@ def main(stdscr):
     renderer.draw_titlescreen()
     cc = get_user_input_ch(stdscr, ['n', 'q'])
     if cc=='n':
-        
         pc = quick_new_character(game, renderer.s)
         pc.y = 0
         pc.x = 0 # when drawing pc or anything in dungeon, have to account for offset of borders
-
         game.pc = pc 
-
         game_loop(game)
     elif cc=='q':
         exit(0)
