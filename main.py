b3d70992d4cff3780e2f189904f7ed60c793bab3
darkmage <165251+mikedesu@users.noreply.github.com>
2022-01-15 21:36:19 -0600

    cleanedup based on linter

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import exit
+# from sys import exit
 from curses import wrapper
-from tools import get_user_input_ch
-from tools import quickNewCharacter 
+# from tools import get_user_input_ch
+from tools import quickNewCharacter
 import Game
 from Game.Renderer import Renderer
-from Game.TitlescreenMenu import TitlescreenMenu 
-from Game.OptionMenu import OptionMenu
-from Game.MessageWindow import MessageWindow
+from Game.TitlescreenMenu import TitlescreenMenu
+# from Game.OptionMenu import OptionMenu
+# from Game.MessageWindow import MessageWindow
+
 
 def gameLoop(game):
     while True:
@@ -19,28 +20,33 @@ def gameLoop(game):
             game.decrOneHungerUnitPC()
             game.incrTurns()
 
+
 def newGame(r):
-    assert(r!=None)
+    assert(r is not None)
     game = Game.Game(renderer=r)
     pc = quickNewCharacter(game, r.s)
     pc.y = 1
-    pc.x = 1 # when drawing pc or anything in dungeon, have to account for offset of borders
-    #pc.hunger = 1
-    game.pc = pc 
+    pc.x = 1
+    # when drawing pc or anything in dungeon, have to account for offset of
+    # borders
+    # pc.hunger = 1
+    game.pc = pc
     gameLoop(game)
+
 
 def constructTitlescreenMenu(renderer):
     renderer.startup()
     title = "darkhack"
     version = "0.01a"
-    titlescreenMenu = TitlescreenMenu( f"{title} {version}", renderer, newGame )
+    titlescreenMenu = TitlescreenMenu(f"{title} {version}", renderer, newGame)
     return titlescreenMenu
+
 
 def main(stdscr):
     renderer = Renderer(screen=stdscr)
     titlescreenMenu = constructTitlescreenMenu(renderer)
     titlescreenMenu.display()
 
-if __name__=='__main__':
-    wrapper(main) # needed for curses
 
+if __name__ == '__main__':
+    wrapper(main)
